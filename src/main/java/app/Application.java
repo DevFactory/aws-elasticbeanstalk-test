ac96411f6e2915a126854540daa11517e65f4f98
Aleksandr Nikiforov <aleksandr.nikiforov@transferwise.com>
2015-10-20 01:31:52 +0300

    Fix deploy to elastic beanstalk

--- src/main/java/app/Application.java
+++ src/main/java/app/Application.java
@@ -1,4 +1,5 @@
 package app;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
@@ -10,5 +11,5 @@ public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
-	
+
 }
